265f8dce2d109cc762f9fbaec702a3fce89a80a1
Dasha  Hello <st.daryaa@gmail.com>
2022-01-27 23:37:38 +0200

    ^ cleanup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './Users/users.module';
 import { PermissionsModule } from './Users/Permissions/permissions.module';
 import { TasksModule } from './Users/Tasks/tasks.module';
@@ -16,8 +14,6 @@ import { AuthModule } from './Users/Auth/auth.module';
     PermissionsModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {
   constructor(private connection: Connection) {}
